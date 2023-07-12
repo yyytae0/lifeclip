b6c6d4a887147fa50547f4f0b9c3da6535e700f6
youn <yyytae0@gmail.com>
2023-07-12 15:50:05 +0900

    pjt setting

---
+++ src/router/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
+function Router() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/"/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+export default Router;
